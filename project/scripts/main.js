23b1832c11474f3f6ad11ea2ece90833f0cee885
Andy Axton <axtonandy@gmail.com>
2014-01-18 10:52:49 -0800

    javascript all the things

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,1 +1,24 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+	  parallax();
+	});
+
+	function parallax(){
+	  var scrolled = $(window).scrollTop();
+	  $('.hero').css('background-position-y', scrolled*0.15 + 'px');
+	}
+
+	var bigbrother = -1;
+
+    $('.team section').each(function() {
+      bigbrother = bigbrother > $('.team section').height() ? bigbrother : $('.team section').height();
+    });
+
+    $('.team section').each(function() {
+      $('.team section').height(bigbrother);
+    });
+	
+});
+
+
 
